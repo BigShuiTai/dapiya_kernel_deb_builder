eae2d4145979d9ea49ee84b1cdbfe74981c530a0
Naga <79071461+BigShuiTai@users.noreply.github.com>
2023-07-02 00:23:12 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -2,37 +2,43 @@
 
 VERSION=$(grep 'Kernel Configuration' < config | awk '{print $3}')
 
-su root
-
 # add deb-src to sources.list
 sed -i "/deb-src/s/# //g" /etc/apt/sources.list
 
 # install dep
-apt update
-apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev git
-apt build-dep -y linux
+sudo apt update
+sudo apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev git lsb vim libelf-dev
+sudo apt build-dep -y linux
 
 # change dir to workplace
 cd "${GITHUB_WORKSPACE}" || exit
 
 # download kernel source
-wget http://www.kernel.org/pub/linux/kernel/v6.x/linux-"$VERSION".tar.xz
-tar -xf linux-"$VERSION".tar.xz
+wget http://www.kernel.org/pub/linux/kernel/v6.x/linux-6.4.tar.gz  
+tar -xf linux-"$VERSION".tar.gz
 cd linux-"$VERSION" || exit
 
 # copy config file
 cp ../config .config
-
+#
 # disable DEBUG_INFO to speedup build
-scripts/config --disable DEBUG_INFO
-
+# scripts/config --disable DEBUG_INFO 
+scripts/config --set-str SYSTEM_TRUSTED_KEYS ""
+scripts/config --set-str SYSTEM_REVOCATION_KEYS ""
+scripts/config --undefine DEBUG_INFO
+scripts/config --undefine DEBUG_INFO_COMPRESSED
+scripts/config --undefine DEBUG_INFO_REDUCED
+scripts/config --undefine DEBUG_INFO_SPLIT
+scripts/config --undefine GDB_SCRIPTS
+scripts/config --set-val  DEBUG_INFO_DWARF5     n
+scripts/config --set-val  DEBUG_INFO_NONE       y
 # apply patches
 # shellcheck source=src/util.sh
 # source ../patch.d/*.sh
 
 # build deb packages
 CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
-make deb-pkg -j"$CPU_CORES"
+sudo make bindeb-pkg -j"$CPU_CORES"
 
 # move deb packages to artifact dir
 cd ..
